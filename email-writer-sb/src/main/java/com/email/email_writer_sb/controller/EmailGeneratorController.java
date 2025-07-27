7838b6cd665b53a3f26656664fba2ed956546749
Akshat Tambe <akshat.tambe@centilytics.com>
2025-07-27 17:02:09 +0530

    Update backend

--- email-writer-sb/src/main/java/com/email/email_writer_sb/controller/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/email_writer_sb/controller/EmailGeneratorController.java
@@ -4,21 +4,23 @@ import com.email.email_writer_sb.dto.EmailRequest;
 import com.email.email_writer_sb.service.EmailGeneratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmailGeneratorController {
 
     @Autowired
     private EmailGeneratorService emailGeneratorService;
 
     @PostMapping("/generate")
-    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest request){
+    public ResponseEntity<Map<String,String>> generateEmail(@RequestBody EmailRequest request){
+        System.out.println("Email Content: " + request.getEmailContent());
+        System.out.println("Tone: " + request.getTone());
         String response = emailGeneratorService.generateEmailReply(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(Map.of("reply",response));
     }
 }
--- email-writer-sb/src/main/java/com/email/email_writer_sb/dto/EmailRequest.java
+++ email-writer-sb/src/main/java/com/email/email_writer_sb/dto/EmailRequest.java
@@ -1,8 +1,10 @@
 package com.email.email_writer_sb.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class EmailRequest {
     private String emailContent;
     private String tone;
